b2ecaee1388efb645bd6120549d701b1eeeeff73
chirprush <chirpyboat73@gmail.com>
2021-02-27 21:06:40 -0500

    Added ability to draw rectangles

--- src/pixels.js
+++ src/pixels.js
@@ -6,6 +6,7 @@ class Pixels {
 		this.cols = cols;
 		this.rows = rows;
 		this.pixels = Array(rows).fill(0).map(_ => Array(cols).fill(0));
+		this.rectStart = null;
 	}
 
 	static get pixl() {
@@ -36,6 +37,16 @@ class Pixels {
 		});
 	}
 
+	fill(start, end, value) {
+		let min = new Vec2(Math.min(start.x, end.x), Math.min(start.y, end.y));
+		let max = new Vec2(Math.max(start.x, end.x), Math.max(start.y, end.y));
+		for (let y = min.y; y <= max.y; ++y) {
+			for (let x = min.x; x <= max.x; ++x) {
+				this.pixels[y][x] = value;
+			}
+		}
+	}
+
 	getAt(pos, offset) {
 		let coord = this.toCoord(pos, offset);
 		if (!this.inBounds(coord)) {
@@ -52,10 +63,18 @@ class Pixels {
 		});
 		let coord = this.toCoord(ctx.mousePos, pos);
 		if (this.inBounds(coord)) ctx.drawRect(new Vec2(coord.x * Pixels.pixl + pos.x, coord.y * Pixels.pixl + pos.y), Pixels.pixl, Pixels.pixl, CURSOR_COLOR);
+		if (this.rectStart) ctx.drawRect(new Vec2(this.rectStart.x * Pixels.pixl + pos.x, this.rectStart.y * Pixels.pixl + pos.y), Pixels.pixl, Pixels.pixl, RECT_COLOR);
 	}
 
 	mouseDown(ctx, button, pos, toolbar) {
-		if (isMobile && toolbar.selected === this.getAt(pos, new Vec(0, 0)) && toolbar.selected > 0) {
+		if (button == 1) {
+			if (this.rectStart === null) {
+				this.rectStart = this.toCoord(pos, new Vec2(0, 0));
+			} else {
+				this.fill(this.rectStart, this.toCoord(pos, new Vec2(0, 0)), toolbar.selected);
+				this.rectStart = null;
+			}
+		} else if (isMobile && toolbar.selected === this.getAt(pos, new Vec(0, 0)) && toolbar.selected > 0) {
 			toolbar.saveSelection = toolbar.selected ? toolbar.selected : toolbar.saveSelection;
 			toolbar.selected = 0;
 		} else if (!isMobile && button == 2) {
@@ -76,5 +95,6 @@ class Pixels {
 
 const BACKGROUND_COLOR = "#1d2021";
 const CURSOR_COLOR = "#61afef";
+const RECT_COLOR = "#98c379";
 
 export { Pixels, BACKGROUND_COLOR, CURSOR_COLOR };
